472d4360f160e45db83d443a20cd1594e20d1ed3
tomato378 <mnwtm100@gmail.com>
2025-11-05 21:46:14 +0900

    Add working ping command for Discord bot

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
-import os
+# bot.py に追加（または修正）
 import discord
 from discord.ext import commands
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 TOKEN = os.getenv("DISCORD_TOKEN")
+
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -17,6 +18,6 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send("🏓 Pong!")
 
 bot.run(TOKEN)
