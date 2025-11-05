2f6443d1336de9806bbf6c10cdf8f53ce9d12a49
tomato378 <mnwtm100@gmail.com>
2025-11-05 09:36:27 +0900

    init commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
